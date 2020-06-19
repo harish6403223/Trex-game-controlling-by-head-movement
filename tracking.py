730e0e57190304a8fbfca936bff6f20a6aa1e3a0
harish6403223 <46072820+harish6403223@users.noreply.github.com>
2020-06-19 13:38:06 +0530

    Update tracking.py

--- tracking.py
+++ tracking.py
@@ -44,52 +44,7 @@ points=[(new.ravel())[0] for i,(new) in enumerate(prevPts) if(new.ravel())[0]>x 
 mask = np.zeros_like(prev_frame)
 
 
-while True:
-
-    # Grab current frame
-    ret,frame = cap.read()
-
-    # Grab gray scale
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Calculate the Optical Flow on the Gray Scale Frame
-    nextPts, status, err = cv2.calcOpticalFlowPyrLK(prev_gray, frame_gray, prevPts, None, **lk_params)
-
-    # Using the returned status array (the status output)
-    # status output status vector (of unsigned chars); each element of the vector is set to 1 if
-    # the flow for the corresponding features has been found, otherwise, it is set to 0.
-    good_new = nextPts[status==1]
-    good_prev = prevPts[status==1]
-
-    # Use ravel to get points to draw lines and circles
-    for i,(new,prev) in enumerate(zip(good_new,good_prev)):
-
-        x_new,y_new = new.ravel()
-        x_prev,y_prev = prev.ravel()
-
-        # Lines will be drawn using the mask created from the first frame
-        mask = cv2.line(mask, (x_new,y_new),(x_prev,y_prev), (0,255,0), 3)
-        cv2.rectangle(mask, (x,y), (x+w,y+h), (255,255,255), 10)
-
-        # Draw red circles at corner points
-        frame = cv2.circle(frame,(x_new,y_new),8,(0,0,255),-1)
-    points1=[(new1.ravel())[0] for i,(new,new1) in enumerate(zip(sl,nextPts)) if(new.ravel())[0]>x and (new.ravel())[0]<(x+w)]
-    for i in points1:
-        if(i<x+h and i>x):
-            s1=0
-        else:
-            s1=1
-            if(i<x and s==0):
-                s=1
-                pyautogui.press('space')
-            elif(s==0):
-                pyautogui.keyDown('down')
-                s=2
-            break
-    if s1==0:
-        if(s==2):
-            pyautogui.keyUp('down')
-        s=0
+##Bug##
 
     # Display the image along with the mask we drew the line on.
     img = cv2.add(frame,mask)
